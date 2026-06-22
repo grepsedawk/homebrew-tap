5822e87e9c84f3ff827a3fdcec1f955d6dd37714
Ally Piechowski <alex@piechowski.io>
2026-06-23 03:34:41 +0800

    i3wm-osx 0.3.0

--- Formula/i3wm-osx.rb
+++ Formula/i3wm-osx.rb
@@ -1,9 +1,9 @@
 class I3wmOsx < Formula
   desc "Tiling window manager for macOS that reads i3 config files"
   homepage "https://github.com/grepsedawk/i3wm-osx"
-  url "https://github.com/grepsedawk/i3wm-osx/releases/download/v0.1.4/i3wm-osx-0.1.4-arm64.tar.gz"
-  version "0.1.4"
-  sha256 "9bef83b790b060708d2c24a448122b2da341c07f0151a1784ae8ddfcf77dfb33"
+  url "https://github.com/grepsedawk/i3wm-osx/releases/download/v0.3.0/i3wm-osx-0.3.0-arm64.tar.gz"
+  version "0.3.0"
+  sha256 "c167203b765eb0f2689f4144f0a250a471a3372d7a3d56c61016ba82736e72dc"
   license "MIT"
 
   depends_on arch: :arm64
